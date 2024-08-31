9cd6adbc2ae811d93f6fdd3ad2302d389ced84e1
Gabriel Lopez <gabriel2004lopezmartinez@gmail.com>
2024-08-31 15:24:07 -0400

    Update Login & SignUp

--- Code/js/cargarRegionesComunas.js
+++ Code/js/cargarRegionesComunas.js
@@ -1,8 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const regionSelect = document.getElementById('region');
-    const comunaSelect = document.getElementById('comuna'); // Corregido aquí
+    const comunaSelect = document.getElementById('comuna'); 
 
-    // Función para cargar el archivo JSON
     async function cargarDatos() {
         try {
             const response = await fetch('data/comunas-regiones.json');
@@ -11,7 +10,6 @@ document.addEventListener('DOMContentLoaded', () => {
             }
             const data = await response.json();
 
-            // Llenar las regiones en el select
             data.regiones.forEach(region => {
                 const option = document.createElement('option');
                 option.value = region.region;
@@ -19,16 +17,13 @@ document.addEventListener('DOMContentLoaded', () => {
                 regionSelect.appendChild(option);
             });
 
-            // Función para actualizar las comunas
             function actualizarComunas() {
                 const regionName = regionSelect.value;
                 const regionData = data.regiones.find(r => r.region === regionName);
                 const comunas = (regionData && regionData.comunas) || [];
 
-                // Limpiar las opciones actuales
                 comunaSelect.innerHTML = '<option value="">Selecciona tu comuna</option>';
 
-                // Agregar las nuevas opciones
                 comunas.forEach(comuna => {
                     const option = document.createElement('option');
                     option.value = comuna;
@@ -36,8 +31,6 @@ document.addEventListener('DOMContentLoaded', () => {
                     comunaSelect.appendChild(option);
                 });
             }
-
-            // Escuchar el cambio en la selección de región
             regionSelect.addEventListener('change', actualizarComunas);
 
         } catch (error) {
