e579f29596fdcd61c7659420654c6d4663277cb9
Gabriel Lopez <gabriel2004lopezmartinez@gmail.com>
2024-09-29 21:01:53 -0300

    Login

--- EP2/Front-End/src/pages/Login/Login.tsx
+++ EP2/Front-End/src/pages/Login/Login.tsx
@@ -1,7 +1,8 @@
-import { IonButton, IonContent, IonHeader, IonInput, IonPage} from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonInput, IonPage, IonToolbar, IonTitle, IonText, IonItem, IonLabel, IonCheckbox} from '@ionic/react';
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
 import './Login.css';
+import logo from './images/logo.svg';
 
 const Login: React.FC = () => {
   const { register , handleSubmit } = useForm();
@@ -16,17 +17,47 @@ const Login: React.FC = () => {
   }
 
   return (
-    <IonPage> 
-      <IonHeader>Inicio de Sesión</IonHeader>
-      <IonContent> 
-        <form onSubmit={handleSubmit(onSubmit)}> 
-          <br/>
-          <IonInput type='email' {...register('email')} placeholder='Correo Electrónico' required /> 
-          <br/>
-          <br/>
-          <IonInput type='password'  {...register('password')} placeholder='Contraseña' required /> 
-          <IonButton type='submit'>Iniciar Sesión</IonButton>
-        </form>
+    <IonPage>
+      <IonContent className="container-main">
+        <div className="logo-container">
+          <img src={logo} alt="logo"/>
+          <div className="logo-text">ClipTask</div>
+        </div>
+          <div className="container-register">
+            <div className="form-container">
+              <h1 id="login">Iniciar Sesión</h1>
+              <IonText color="medium" className="register-link">
+                ¿Es tu primera vez? <a href="signup.html">Regístrate</a>
+              </IonText>
+              <br/><br/>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <IonLabel position="floating">Correo electrónico</IonLabel>
+                <IonItem className='formInput'>
+                  <IonInput type="email" {...register('email')} required placeholder="Correo electrónico" />
+                </IonItem>
+                <IonText id="nombre-error" className="text-danger"></IonText>
+                <br/>
+                <IonLabel position="floating">Contraseña</IonLabel>
+                <IonItem className='formInput'>
+                  <IonInput type="password" {...register('password')} required placeholder="Contraseña" />
+                </IonItem>
+                <IonText id="contraseña-error" className="text-danger"></IonText>
+
+                <p className="forgot-password">
+                  <a href="#">¿Olvidaste la contraseña?</a>
+                </p>
+
+                <div className="form-group rememberme">
+                  <IonCheckbox id="rememberme" />
+                  <IonLabel>Recuérdame</IonLabel>
+                </div>
+
+                <div className="btn-login-container">
+                  <IonButton expand="full" shape='round' size='large' type='submit' id="login-button" className="btn-login">INICIAR SESIÓN</IonButton>
+                </div>
+              </form>
+            </div>
+          </div>
       </IonContent>
     </IonPage>
   );
