26593537879df0208fab8b0cc33f1981e62eee6b
Gabriel Lopez <gabriel2004lopezmartinez@gmail.com>
2024-10-16 00:11:51 -0300

    Update NavBar.tsx

--- EP2/Front-End/src/components/NavBar.tsx
+++ EP2/Front-End/src/components/NavBar.tsx
@@ -1,27 +1,31 @@
-import React from 'react';
-import { IonTabButton, IonTabBar, IonLabel, IonIcon } from '@ionic/react';
-import { homeOutline, folderOutline, notificationsOutline } from 'ionicons/icons';
-import './navBar.css'
+import React from "react";
+import { IonTabButton, IonTabBar, IonLabel, IonIcon } from "@ionic/react";
+import {
+  homeOutline,
+  folderOutline,
+  notificationsOutline,
+} from "ionicons/icons";
+import "./navBar.css";
 
-interface ContainerNavBar { }
+interface ContainerNavBar {}
 
 const NavBar: React.FC<ContainerNavBar> = () => {
   return (
     <IonTabBar slot="bottom">
-        <IonTabButton tab="home" href="/home">
-            <IonIcon icon={homeOutline} />
-            <IonLabel>Inicio</IonLabel>
-        </IonTabButton>
-        <IonTabButton tab="projects" href="/projects">
-            <IonIcon icon={folderOutline} />
-            <IonLabel>Proyectos</IonLabel>
-        </IonTabButton>
-        <IonTabButton tab="notifications" href="/notifications">
-            <IonIcon icon={notificationsOutline} />
-            <IonLabel>Notis.</IonLabel>
-        </IonTabButton>
+      <IonTabButton tab="home" href="/home">
+        <IonIcon icon={homeOutline} />
+        <IonLabel>Inicio</IonLabel>
+      </IonTabButton>
+      <IonTabButton tab="projects" href="/projects">
+        <IonIcon icon={folderOutline} />
+        <IonLabel>Proyectos</IonLabel>
+      </IonTabButton>
+      <IonTabButton tab="notifications" href="/notifications">
+        <IonIcon icon={notificationsOutline} />
+        <IonLabel>Notis.</IonLabel>
+      </IonTabButton>
     </IonTabBar>
-    );
+  );
 };
 
 export default NavBar;
